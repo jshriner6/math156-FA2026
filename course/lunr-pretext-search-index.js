27f50af4823a420f22ebbcf24de9d8ba81877735
Jeff <110954699+jshriner6@users.noreply.github.com>
2026-08-17 17:32:13 +0000

    Latest build deployed.

--- course/lunr-pretext-search-index.js
+++ course/lunr-pretext-search-index.js
@@ -42,8 +42,8 @@ var ptx_lunr_docs = [
   "url": "sec-I-1.html",
   "type": "Worksheet",
   "number": "",
-  "title": "Course Motivation and <span class=\"process-math\">\\(\\S I.1\\text{:}\\)<\/span> Sets and Elements",
-  "body": " Course Motivation and : Sets and Elements    Why? Traditional Calculus vs. Math 156      Motivation: Sets and Elements      Definition: Set   A set is      The following exercises will introduce the important notation (i.e., symbols) used to to communicate sets, and the various ways we can build them.   Activity: Sets and Elements   The following are examples of sets. Use them to answer the questions.                                     Do you think any sets are the same? Explain your reasoning.      The following express TRUE statements. Write out what you think they mean using an English sentence.                            What questions or observations do you have about sets so far?        Activity: Set Builder Notation   For large or more complicated sets, it's not always convenient (or possible) to write out every element of the set as we did previously. To communicate these sets, we can use set builder notation . The following are examples of sets using set builder notation. Use them to answer the questions.                                It is true that . Write an English sentence that describes set builder notation for .      For , write out the sets explicitly, or describe what they are in more common language. Are any sets equal?      Determine if the following are TRUE or FALSE.                    Some Special Sets   The following are commonly used sets that have special notation:                           Practice: True or False?                                        "
+  "title": "Setting the Stage and <span class=\"process-math\">\\(\\S I.1\\text{:}\\)<\/span> Sets and Elements",
+  "body": " Setting the Stage and : Sets and Elements    Why? Traditional Calculus vs. Math 156      Motivation: Sets and Elements      Definition: Set   A set is      The following exercises will introduce the important notation (i.e., symbols) used to to communicate sets, and the various ways we can build them.   Activity: Sets and Elements   The following are examples of sets. Use them to answer the questions.                                     Do you think any sets are the same? Explain your reasoning.      The following express TRUE statements. Write out what you think they mean using an English sentence.                            What questions or observations do you have about sets so far?        Activity: Set Builder Notation   For large or more complicated sets, it's not always convenient (or possible) to write out every element of the set as we did previously. To communicate these sets, we can use set builder notation . The following are examples of sets using set builder notation. Use them to answer the questions.                                It is true that . Write an English sentence that describes set builder notation for .      For , write out the sets explicitly, or describe what they are in more common language. Are any sets equal?      Determine if the following are TRUE or FALSE.                    Some Special Sets   The following are commonly used sets that have special notation:                           Practice: True or False?                                        "
 },
 {
   "id": "xr-why-intro",
@@ -107,6 +107,285 @@ var ptx_lunr_docs = [
   "number": "7",
   "title": "Practice: True or False?",
   "body": " Practice: True or False?                                      "
+},
+{
+  "id": "sec-I-2-3-6",
+  "level": "1",
+  "url": "sec-I-2-3-6.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "<span class=\"process-math\">\\(\\S I.2,3,6\\text{:}\\)<\/span> Sets From Sets",
+  "body": " : Sets From Sets    Motivation: Sets from Sets   There are many useful ways to obtain sets from other sets. Intuitively, we may want to:     Definition: Subset  Let and be sets. is a subset of if    Example: Subsets  Let . What are all of the subsets of ?    Useful Fact: Subsets and Equality          Activity: Subsets   Let . Determine if the following are TRUE or FALSE.                                          Definition: Power Set   Let be a set. The power set of is     Example: Power Set         Activity: Power Set Cardinality             A venn diagram. Two overlapping circles, one labeled A and one labeled B.                         Activity: Union, Difference and Intersection    Let and . Compute                            Let and be sets. Determine if the following are TRUE or FALSE.                                             Example: Distributive Properties         Activity: Distributive Properties         Note: Universal Sets and Complements        Example: DeMorgan's Laws   Let and be subsets of a universal set .      Activity: DeMorgan's Laws           Ordered Pairs        Definition: Cartesian Product   Given sets and , the Cartesian product of and is     Example: Cartesian Product   Let and . Then      Activity: Cardinality of Cartesian Products           Tuples and Index sets          Multisets        "
+},
+{
+  "id": "sec-I-2-3-6-2-1",
+  "level": "2",
+  "url": "sec-I-2-3-6.html#sec-I-2-3-6-2-1",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "Motivation: Sets from Sets.",
+  "body": " Motivation: Sets from Sets   There are many useful ways to obtain sets from other sets. Intuitively, we may want to:   "
+},
+{
+  "id": "sec-I-2-3-6-2-2",
+  "level": "2",
+  "url": "sec-I-2-3-6.html#sec-I-2-3-6-2-2",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "Definition: Subset.",
+  "body": " Definition: Subset  Let and be sets. is a subset of if  "
+},
+{
+  "id": "sec-I-2-3-6-2-3",
+  "level": "2",
+  "url": "sec-I-2-3-6.html#sec-I-2-3-6-2-3",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "Example: Subsets.",
+  "body": " Example: Subsets  Let . What are all of the subsets of ?  "
+},
+{
+  "id": "sec-I-2-3-6-2-4",
+  "level": "2",
+  "url": "sec-I-2-3-6.html#sec-I-2-3-6-2-4",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "Useful Fact: Subsets and Equality.",
+  "body": " Useful Fact: Subsets and Equality      "
+},
+{
+  "id": "sec-I-2-3-6-3-1",
+  "level": "2",
+  "url": "sec-I-2-3-6.html#sec-I-2-3-6-3-1",
+  "type": "Worksheet Exercise",
+  "number": "5",
+  "title": "Activity: Subsets.",
+  "body": " Activity: Subsets   Let . Determine if the following are TRUE or FALSE.                                        "
+},
+{
+  "id": "sec-I-2-3-6-3-2",
+  "level": "2",
+  "url": "sec-I-2-3-6.html#sec-I-2-3-6-3-2",
+  "type": "Worksheet Exercise",
+  "number": "6",
+  "title": "Definition: Power Set.",
+  "body": " Definition: Power Set   Let be a set. The power set of is   "
+},
+{
+  "id": "sec-I-2-3-6-3-3",
+  "level": "2",
+  "url": "sec-I-2-3-6.html#sec-I-2-3-6-3-3",
+  "type": "Worksheet Exercise",
+  "number": "7",
+  "title": "Example: Power Set.",
+  "body": " Example: Power Set       "
+},
+{
+  "id": "sec-I-2-3-6-3-4",
+  "level": "2",
+  "url": "sec-I-2-3-6.html#sec-I-2-3-6-3-4",
+  "type": "Worksheet Exercise",
+  "number": "8",
+  "title": "Activity: Power Set Cardinality.",
+  "body": " Activity: Power Set Cardinality       "
+},
+{
+  "id": "sec-I-2-3-6-4-1",
+  "level": "2",
+  "url": "sec-I-2-3-6.html#sec-I-2-3-6-4-1",
+  "type": "Worksheet Exercise",
+  "number": "9",
+  "title": "",
+  "body": "   A venn diagram. Two overlapping circles, one labeled A and one labeled B.                       "
+},
+{
+  "id": "sec-I-2-3-6-4-2",
+  "level": "2",
+  "url": "sec-I-2-3-6.html#sec-I-2-3-6-4-2",
+  "type": "Worksheet Exercise",
+  "number": "10",
+  "title": "Activity: Union, Difference and Intersection.",
+  "body": " Activity: Union, Difference and Intersection    Let and . Compute                            Let and be sets. Determine if the following are TRUE or FALSE.                                         "
+},
+{
+  "id": "sec-I-2-3-6-5-1",
+  "level": "2",
+  "url": "sec-I-2-3-6.html#sec-I-2-3-6-5-1",
+  "type": "Worksheet Exercise",
+  "number": "11",
+  "title": "Example: Distributive Properties.",
+  "body": " Example: Distributive Properties       "
+},
+{
+  "id": "sec-I-2-3-6-5-2",
+  "level": "2",
+  "url": "sec-I-2-3-6.html#sec-I-2-3-6-5-2",
+  "type": "Worksheet Exercise",
+  "number": "12",
+  "title": "Activity: Distributive Properties.",
+  "body": " Activity: Distributive Properties       "
+},
+{
+  "id": "sec-I-2-3-6-5-3",
+  "level": "2",
+  "url": "sec-I-2-3-6.html#sec-I-2-3-6-5-3",
+  "type": "Worksheet Exercise",
+  "number": "13",
+  "title": "Note: Universal Sets and Complements.",
+  "body": " Note: Universal Sets and Complements      "
+},
+{
+  "id": "sec-I-2-3-6-5-4",
+  "level": "2",
+  "url": "sec-I-2-3-6.html#sec-I-2-3-6-5-4",
+  "type": "Worksheet Exercise",
+  "number": "14",
+  "title": "Example: DeMorgan’s Laws.",
+  "body": " Example: DeMorgan's Laws   Let and be subsets of a universal set .    "
+},
+{
+  "id": "sec-I-2-3-6-5-5",
+  "level": "2",
+  "url": "sec-I-2-3-6.html#sec-I-2-3-6-5-5",
+  "type": "Worksheet Exercise",
+  "number": "15",
+  "title": "Activity: DeMorgan’s Laws.",
+  "body": " Activity: DeMorgan's Laws       "
+},
+{
+  "id": "sec-I-2-3-6-6-1",
+  "level": "2",
+  "url": "sec-I-2-3-6.html#sec-I-2-3-6-6-1",
+  "type": "Worksheet Exercise",
+  "number": "16",
+  "title": "Ordered Pairs.",
+  "body": " Ordered Pairs      "
+},
+{
+  "id": "sec-I-2-3-6-6-2",
+  "level": "2",
+  "url": "sec-I-2-3-6.html#sec-I-2-3-6-6-2",
+  "type": "Worksheet Exercise",
+  "number": "17",
+  "title": "Definition: Cartesian Product.",
+  "body": " Definition: Cartesian Product   Given sets and , the Cartesian product of and is   "
+},
+{
+  "id": "sec-I-2-3-6-6-3",
+  "level": "2",
+  "url": "sec-I-2-3-6.html#sec-I-2-3-6-6-3",
+  "type": "Worksheet Exercise",
+  "number": "18",
+  "title": "Example: Cartesian Product.",
+  "body": " Example: Cartesian Product   Let and . Then    "
+},
+{
+  "id": "sec-I-2-3-6-6-4",
+  "level": "2",
+  "url": "sec-I-2-3-6.html#sec-I-2-3-6-6-4",
+  "type": "Worksheet Exercise",
+  "number": "19",
+  "title": "Activity: Cardinality of Cartesian Products.",
+  "body": " Activity: Cardinality of Cartesian Products       "
+},
+{
+  "id": "sec-I-2-3-6-7-1",
+  "level": "2",
+  "url": "sec-I-2-3-6.html#sec-I-2-3-6-7-1",
+  "type": "Worksheet Exercise",
+  "number": "20",
+  "title": "Tuples and Index sets.",
+  "body": " Tuples and Index sets      "
+},
+{
+  "id": "sec-I-2-3-6-8-1",
+  "level": "2",
+  "url": "sec-I-2-3-6.html#sec-I-2-3-6-8-1",
+  "type": "Worksheet Exercise",
+  "number": "21",
+  "title": "Multisets.",
+  "body": " Multisets      "
+},
+{
+  "id": "sec-I-4",
+  "level": "1",
+  "url": "sec-I-4.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "<span class=\"process-math\">\\(\\S I.4\\text{:}\\)<\/span> Connections to (classical) Logic",
+  "body": " : Connections to (classical) Logic    Motivation: Logic        Propositions and Operations        Truth Tables   Operations on propositions result in new propositions. To keep track of the resulting truth values, we can use truth tables .         T     F                                             Logic and Sets        Conditional Statements   We can combine propositions in two more important ways:     Example: Determining truth values of complicated propositions   Determine the truth table for the proposition .       Important Conditional Statements   For the conditional statement , we can form three other (related) conditional statements:     Practice    Let be the proposition \"It is snowing\" and be the proposition \"CSU is closed for the day\". Translate the following to English sentences.                            Use truth tables to show that is logically equivalent (i.e., has the same truth table) to .      Without using truth tables, explain why the inverse and converse of are logically equivalent.      "
+},
+{
+  "id": "sec-I-4-2-1",
+  "level": "2",
+  "url": "sec-I-4.html#sec-I-4-2-1",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "Motivation: Logic.",
+  "body": " Motivation: Logic      "
+},
+{
+  "id": "sec-I-4-2-2",
+  "level": "2",
+  "url": "sec-I-4.html#sec-I-4-2-2",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "Propositions and Operations.",
+  "body": " Propositions and Operations      "
+},
+{
+  "id": "sec-I-4-2-3",
+  "level": "2",
+  "url": "sec-I-4.html#sec-I-4-2-3",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "Truth Tables.",
+  "body": " Truth Tables   Operations on propositions result in new propositions. To keep track of the resulting truth values, we can use truth tables .         T     F                                         "
+},
+{
+  "id": "sec-I-4-3-1",
+  "level": "2",
+  "url": "sec-I-4.html#sec-I-4-3-1",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "Logic and Sets.",
+  "body": " Logic and Sets      "
+},
+{
+  "id": "sec-I-4-3-2",
+  "level": "2",
+  "url": "sec-I-4.html#sec-I-4-3-2",
+  "type": "Worksheet Exercise",
+  "number": "5",
+  "title": "Conditional Statements.",
+  "body": " Conditional Statements   We can combine propositions in two more important ways:   "
+},
+{
+  "id": "sec-I-4-3-3",
+  "level": "2",
+  "url": "sec-I-4.html#sec-I-4-3-3",
+  "type": "Worksheet Exercise",
+  "number": "6",
+  "title": "Example: Determining truth values of complicated propositions.",
+  "body": " Example: Determining truth values of complicated propositions   Determine the truth table for the proposition .   "
+},
+{
+  "id": "sec-I-4-4-1",
+  "level": "2",
+  "url": "sec-I-4.html#sec-I-4-4-1",
+  "type": "Worksheet Exercise",
+  "number": "7",
+  "title": "Important Conditional Statements.",
+  "body": " Important Conditional Statements   For the conditional statement , we can form three other (related) conditional statements:   "
+},
+{
+  "id": "sec-I-4-4-2",
+  "level": "2",
+  "url": "sec-I-4.html#sec-I-4-4-2",
+  "type": "Worksheet Exercise",
+  "number": "8",
+  "title": "Practice.",
+  "body": " Practice    Let be the proposition \"It is snowing\" and be the proposition \"CSU is closed for the day\". Translate the following to English sentences.                            Use truth tables to show that is logically equivalent (i.e., has the same truth table) to .      Without using truth tables, explain why the inverse and converse of are logically equivalent.    "
 }
 ]
 
